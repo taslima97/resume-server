1a41faeff7e691b28fcee12f7528d0420d115df1
taslima97 <antora.taslima97@gmail.com>
2023-05-21 20:05:10 +0600

    delete method

--- index.js
+++ index.js
@@ -68,6 +68,15 @@ async function run() {
       res.send(result)
     })
 
+
+app.delete('/insertToy/:id',async(req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await toysCollection.deleteOne(query);
+  res.send(result)
+})
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
